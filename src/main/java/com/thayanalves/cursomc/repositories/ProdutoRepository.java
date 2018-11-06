5780202f05da0c427cd54cf9dfcd21a9880e8614
Thayan Alves <u-chihanosasuke@hotmail.com>
2018-11-05 23:39:59 -0200

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/thayanalves/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.thayanalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thayanalves.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
